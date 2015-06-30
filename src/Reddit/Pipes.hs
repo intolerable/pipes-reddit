39e30567d4193c6fe29ca5dff95fc0c79eba70e8
Fraser Murray <fraser.m.murray@gmail.com>
2015-06-30 13:32:32 +0100

    fix breaking on delete

--- src/Reddit/Pipes.hs
+++ src/Reddit/Pipes.hs
@@ -7,36 +7,37 @@ module Reddit.Pipes
 import Control.Applicative
 import Control.Concurrent (threadDelay)
 import Control.Monad.IO.Class
+import Data.Function
 import Pipes (Producer)
+import Prelude
 import Reddit
 import Reddit.Types.Comment
-import Reddit.Types.Post
 import Reddit.Types.Listing hiding (before, after)
 import Reddit.Types.Options
+import Reddit.Types.Post
 import Reddit.Types.Subreddit
 import Reddit.Types.User
 import qualified Pipes
+import qualified Data.BoundedSet as Bounded
 
 type Interval = Int
 
-redditStream :: MonadIO m => Maybe SubredditName -> Maybe a -> Interval -> (b -> a) -> (Options a -> Maybe SubredditName -> RedditT m (Listing c b)) -> Producer b (RedditT m) ()
-redditStream sub pid interval f g = do
-  Listing _ _ xs <- Pipes.lift $ g (Options (Before <$> pid) (Just 100)) sub
-  mapM_ Pipes.yield xs
-  case xs of
-    [] -> do
-      liftIO $ threadDelay $ interval * 1000 * 1000
-      redditStream sub pid interval f g
-    y : _ -> do
-      redditStream sub (Just $ f y) interval f g
+redditStream :: (Ord a, MonadIO m) => Maybe SubredditName -> Interval -> (b -> a) -> (Options a -> Maybe SubredditName -> RedditT m (Listing c b)) -> Producer b (RedditT m) ()
+redditStream sub interval f g = flip fix (Bounded.empty 200) $ \loop s -> do
+  liftIO $ print $ Bounded.size s
+  Listing _ _ xs <- Pipes.lift $ g (Options Nothing (Just 100)) sub
+  let news = filter (\x -> not $ Bounded.member (f x) s) xs
+  mapM_ Pipes.yield news
+  liftIO $ threadDelay $ interval * 1000 * 1000
+  loop (Bounded.insertAll (map f news) s)
 
 postStream :: MonadIO m => Maybe SubredditName -> Interval -> Producer Post (RedditT m) ()
 postStream sub interval =
-  redditStream sub Nothing interval postID (\o s -> getPosts' o New s)
+  redditStream sub interval postID (\o s -> getPosts' o New s)
 
 commentStream :: MonadIO m => Maybe SubredditName -> Interval -> Producer Comment (RedditT m) ()
 commentStream sub interval =
-  redditStream sub Nothing interval commentID getNewComments'
+  redditStream sub interval commentID getNewComments'
 
 allUserComments :: MonadIO m => Username -> Producer Comment (RedditT m) ()
 allUserComments username = go (Just Nothing)
